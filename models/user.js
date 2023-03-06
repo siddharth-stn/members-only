357f1d82a034f94907f8970757ba627883084967
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-03-06 20:29:15 +0530

    user model created

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  family_name: { type: String, required: true, maxLength: 100 },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  membershipStatus: {
+    type: String,
+    required: true,
+    enum: ["Normal", "Club Member"],
+    default: "Normal",
+  },
+  isAdmin: {
+    type: String,
+    required: true,
+    enum: [true, false],
+    default: false,
+  },
+  message: [{ type: Schema.Types.ObjectId, ref: "Message" }],
+});
+
+UserSchema.virtual("name").get(function () {
+  return `${this.first_name} ${this.family_name}`;
+});
+
+UserSchema.virtual("url").get(function () {
+  return `/story/user/${this._id}`;
+});
+
+module.exports = mongoose.model("User", UserSchema);
