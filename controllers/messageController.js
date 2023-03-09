6dd0eb1cfd4b79b57da1e5471ee76cec0e714fa7
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-03-09 18:24:07 +0530

    create new message done

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,3 +1,4 @@
+const { body, validationResult } = require("express-validator");
 const Message = require("../models/message");
 
 exports.message_list_get = (req, res, next) => {
@@ -24,14 +25,59 @@ exports.message_list_get = (req, res, next) => {
 };
 
 exports.message_create_get = (req, res, next) => {
-  res.render("create-message", {
-    title: "Create Message",
-  });
+  if (req.user) {
+    res.render("create-message", {
+      title: "Create Message",
+    });
+    return;
+  } else {
+    const error = new Error("Only a registered user can create message");
+    error.status = 404;
+    res.render("create-message", {
+      title: "Create Message",
+      error,
+    });
+  }
 };
 
-exports.message_create_post = (req, res, next) => {
-  res.send("Not Implemented: Create new message POST");
-};
+exports.message_create_post = [
+  // validate and sanitize fields
+  body("title").trim().isLength({ min: 1 }).escape(),
+  body("text").trim().isLength({ min: 1 }).escape(),
+
+  (req, res, next) => {
+    const anotherError = validationResult(req);
+
+    console.log(req.user._id);
+    const message = new Message({
+      author: req.user._id,
+      title: req.body.title,
+      text: req.body.text,
+    });
+
+    if (!req.user || !anotherError.isEmpty()) {
+      let error;
+      if (!req.user) {
+        error = new Error(
+          "Only Registered Users are allowed to create a message"
+        );
+      }
+      res.render("create-message", {
+        title: "Create Message",
+        error,
+        anotherError,
+      });
+      return;
+    }
+    message
+      .save()
+      .then(() => {
+        res.redirect("/story/message-list");
+        return;
+      })
+      .catch(next);
+  },
+];
 
 exports.message_delete_post = (req, res, next) => {
   res.send("Not Implemented: Delete messages POST");
